a709a73604037dfe45400d3f586c6dad3e73f3ef
CalmSun <128170708+CalmSun@users.noreply.github.com>
2025-10-23 13:40:57 +0800

    Update config.js

--- config.js
+++ config.js
@@ -2,32 +2,20 @@ const config = {
   mode: "in", // 签到:in,签退:out
   accounts: [
     {
-      username: "", //用户名
-      password: "", //密码
+      username: "18379230102", //用户名
+      password: "ch15350214525", //密码
       openId: "", //微信小程序抓包openid(可选)
       unionId: "", //微信小程序抓包unionId(可选)
       sign: true, //是否自动签到
       reSign: false, //是否重新签到
-      location: "", //经纬度 (可选),例如 "120.210792,30.246026"、"经度,纬度",不填写则自动获取（推荐）
+      location: "121.50,30.99", //经纬度 (可选),例如 "120.210792,30.246026"、"经度,纬度",不填写则自动获取（推荐）
       // signImagePath: "./images/1.jpeg", //签到图片
-      // needReport: false, //是否自动填写周报
-    },
-    // 添加多个账户
-    // {
-    //   username: "",
-    //   password: "",
-    //   openId: "",
-    //   unionId: "",
-    //   sign: true, //是否自动签到
-    //   reSign: true, //是否重新签到
-    //   signImagePath: "./images/1.jpeg", //签到图片
-    //   needReport: false, //是否自动填写周报
-    // },
+      needReport: false, //是否自动填写周报
+    }
   ],
   qmsgKey: "", //qmsg酱key
   qmsgTo: "", //推送的qq号,用,分隔(可选)
-  wxPusherAppToken: "", // wxPusher 的 appToken
-  // TODO: feat: (wxpusher/topic) support wxpusher/topic push
+  wxPusherAppToken: "SPT_tIjyFzjc1Oq9Bvmr46l7vRht100b", // wxPusher 的 appToken
 };
 
 const modeCN = {
